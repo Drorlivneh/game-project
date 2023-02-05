905d79da4b6c0062657fb180c5ed0ef090b44c03
DrorLivneh <Drorlivneh@gmail.com>
2023-02-05 09:00:25 +0200

    game-project to push to GitHab

---
+++ main.js
@@ -0,0 +1,153 @@
+function ReturnToStepOne() {
+    var middleDivStepTwo = document.getElementById('middle-div-step-two');
+    middleDivStepTwo === null || middleDivStepTwo === void 0 ? void 0 : middleDivStepTwo.remove();
+    // middleDivStepTwo.style.display = 'none';
+    // middleDivStepTwo?.innerHTML = '';
+    // map.remove();
+    stepOne.style.display = 'block';
+}
+var scores = 0;
+var pointNumber = document.getElementById('points-number');
+var stepOne = document.getElementById('middle-div-step-one');
+//#############################eventlisteners##########################
+var scissors = document.getElementById('scissors');
+scissors.addEventListener('click', function (e) {
+    e.preventDefault();
+    stepOne.style.display = 'none';
+    presentStepTwo(scissors);
+    pointNumber === null || pointNumber === void 0 ? void 0 : pointNumber.innerHTML = scores;
+    if (scores < 2) {
+        var myTimeout = setTimeout(ReturnToStepOne, 1000);
+    }
+});
+var rock = document.getElementById('rock');
+rock.addEventListener('click', function (e) {
+    e.preventDefault();
+    var yourCoise = "";
+    stepOne.style.display = 'none';
+    presentStepTwo(rock);
+    pointNumber === null || pointNumber === void 0 ? void 0 : pointNumber.innerHTML = scores;
+    if (scores < 2) {
+        var myTimeout = setTimeout(ReturnToStepOne, 1000);
+    }
+});
+var paper = document.getElementById('paper');
+paper.addEventListener('click', function (e) {
+    e.preventDefault();
+    var yourCoise = "";
+    stepOne.style.display = 'none';
+    presentStepTwo(paper);
+    pointNumber === null || pointNumber === void 0 ? void 0 : pointNumber.innerHTML = scores;
+    if (scores < 2) {
+        var myTimeout = setTimeout(ReturnToStepOne, 1000);
+    }
+});
+function presentStepTwo(yourChoise) {
+    // let yourChoiceSvg:string;
+    // if(yourChoise===scissors){
+    // yourChoiceSvg = 
+    var borderColor = 'rgb(255, 170, 0)';
+    var middleDiv = document.getElementById('middle-div');
+    //containers
+    var middleDivStepTwo = document.createElement("div");
+    middleDivStepTwo.id = 'middle-div-step-two';
+    var youPickedContainer = document.createElement("div");
+    youPickedContainer.id = 'you-picked-container';
+    var theHousePickedContainer = document.createElement("div");
+    theHousePickedContainer.id = 'the-house-picked-container';
+    //inside containers-titles
+    var youPickedTitle = document.createElement("div");
+    youPickedTitle.id = 'you-picked-title';
+    youPickedTitle.innerHTML = 'you picked';
+    var theHousePickedTitle = document.createElement("div");
+    theHousePickedTitle.id = 'the-house-picked-title';
+    theHousePickedTitle.innerHTML = 'The house picked';
+    //inside containers-logos
+    var youPickedLogo = document.createElement("img");
+    youPickedLogo.className = 'button-step-two';
+    var yourChoiseInNumber;
+    switch (yourChoise) {
+        case rock: {
+            youPickedLogo.style.borderColor = 'hsl(349, 71%, 52%)';
+            youPickedLogo.src = "./images/icon-rock.svg";
+            yourChoiseInNumber = 1;
+            break;
+        }
+        case paper: {
+            youPickedLogo.style.borderColor = 'hsl(230, 89%, 62%)';
+            youPickedLogo.src = "./images/icon-paper.svg";
+            yourChoiseInNumber = 2;
+            break;
+        }
+        case scissors: {
+            youPickedLogo.style.borderColor = 'rgb(255, 170, 0)';
+            youPickedLogo.src = "./images/icon-scissors.svg";
+            yourChoiseInNumber = 3;
+            break;
+        }
+    }
+    // if(yourChoise===scissors){
+    //     youPickedLogo.style.borderColor = 'rgb(255, 170, 0)';
+    //     youPickedLogo.style.borderColor = 'hsl(230, 89%, 62%)';
+    //     youPickedLogo.style.borderColor = 'hsl(349, 71%, 52%)';
+    //     // youPickedLogo.src = './images/icon-scissors.svg'
+    //     // youPickedLogo.innerHTML = `<${yourChoice}>`
+    // }
+    var theHousePickedLogo = document.createElement("img");
+    theHousePickedLogo.className = 'button-step-two';
+    var computerChoice = Math.round(Math.random() * (3 - 1) + 1);
+    console.log(computerChoice);
+    if (computerChoice === 1) {
+        theHousePickedLogo.style.borderColor = 'hsl(349, 71%, 52%)';
+        theHousePickedLogo.src = "./images/icon-rock.svg";
+        // theHousePickedLogo.width = '22'
+    }
+    if (computerChoice === 2) {
+        theHousePickedLogo.style.borderColor = 'hsl(230, 89%, 62%)';
+        theHousePickedLogo.src = "./images/icon-paper.svg";
+    }
+    if (computerChoice === 3) {
+        theHousePickedLogo.style.borderColor = 'rgb(255, 170, 0)';
+        theHousePickedLogo.src = "./images/icon-scissors.svg";
+    }
+    //#########################calulating win and points#################
+    if (yourChoiseInNumber === computerChoice) {
+        var message = "It's a tie";
+    }
+    if (yourChoiseInNumber === 1 && computerChoice === 2) {
+        var message = "you lose";
+        scores -= 1;
+    }
+    if (yourChoiseInNumber === 2 && computerChoice === 3) {
+        var message = "you lose";
+        scores -= 1;
+    }
+    if (yourChoiseInNumber === 3 && computerChoice === 1) {
+        var message = "you lose";
+        scores -= 1;
+    }
+    if (yourChoiseInNumber === 2 && computerChoice === 1) {
+        var message = "you win";
+        scores += 1;
+    }
+    if (yourChoiseInNumber === 3 && computerChoice === 2) {
+        scores += 1;
+    }
+    if (yourChoiseInNumber === 1 && computerChoice === 3) {
+        scores += 1;
+    }
+    middleDiv === null || middleDiv === void 0 ? void 0 : middleDiv.appendChild(middleDivStepTwo);
+    middleDivStepTwo === null || middleDivStepTwo === void 0 ? void 0 : middleDivStepTwo.appendChild(youPickedContainer);
+    if (scores >= 2) {
+        var youWinContainer = document.createElement("div");
+        youWinContainer.id = 'you-win-container';
+        youWinContainer.innerHTML = 'YOU WIN';
+        middleDivStepTwo === null || middleDivStepTwo === void 0 ? void 0 : middleDivStepTwo.appendChild(youWinContainer);
+    }
+    middleDivStepTwo === null || middleDivStepTwo === void 0 ? void 0 : middleDivStepTwo.appendChild(theHousePickedContainer);
+    youPickedContainer === null || youPickedContainer === void 0 ? void 0 : youPickedContainer.appendChild(youPickedTitle);
+    youPickedContainer === null || youPickedContainer === void 0 ? void 0 : youPickedContainer.appendChild(youPickedLogo);
+    theHousePickedContainer === null || theHousePickedContainer === void 0 ? void 0 : theHousePickedContainer.appendChild(theHousePickedTitle);
+    theHousePickedContainer === null || theHousePickedContainer === void 0 ? void 0 : theHousePickedContainer.appendChild(theHousePickedLogo);
+    console.log(scores);
+}
---
+++ main.ts
@@ -0,0 +1,171 @@
+
+function ReturnToStepOne(){
+    let middleDivStepTwo = document.getElementById('middle-div-step-two') as HTMLDivElement | null;
+    middleDivStepTwo?.remove();
+    // middleDivStepTwo.style.display = 'none';
+    // middleDivStepTwo?.innerHTML = '';
+    // map.remove();
+    
+    stepOne.style.display = 'block';
+}
+
+let scores:number = 0;
+let pointNumber = document.getElementById('points-number') as HTMLDivElement | null;
+let stepOne = document.getElementById('middle-div-step-one') as HTMLDivElement | null;
+//#############################eventlisteners##########################
+let scissors = document.getElementById('scissors') as HTMLDivElement | null;
+scissors.addEventListener('click', function(e){
+    e.preventDefault();
+    stepOne.style.display = 'none';
+    presentStepTwo(scissors);
+    pointNumber?.innerHTML = scores;
+    if (scores<2){
+    const myTimeout = setTimeout(ReturnToStepOne, 1000);
+}
+})
+let rock = document.getElementById('rock') as HTMLDivElement | null;
+rock.addEventListener('click', function(e){
+    e.preventDefault();
+    let yourCoise:string = ""
+    stepOne.style.display = 'none';
+    presentStepTwo(rock);
+    pointNumber?.innerHTML = scores;
+    if (scores<2){
+        const myTimeout = setTimeout(ReturnToStepOne, 1000);
+    }
+})
+
+let paper = document.getElementById('paper') as HTMLDivElement | null;
+paper.addEventListener('click', function(e){
+    e.preventDefault();
+    let yourCoise:string = ""
+    stepOne.style.display = 'none';
+    presentStepTwo(paper);
+    pointNumber?.innerHTML = scores;
+    if (scores<2){
+        const myTimeout = setTimeout(ReturnToStepOne, 1000);
+    }
+})
+
+
+
+
+
+function presentStepTwo(yourChoise:HTMLDivElement | null){
+    // let yourChoiceSvg:string;
+    // if(yourChoise===scissors){
+        // yourChoiceSvg = 
+        let borderColor:String = 'rgb(255, 170, 0)';
+    let middleDiv = document.getElementById('middle-div') as HTMLDivElement | null;
+    //containers
+    let middleDivStepTwo = document.createElement("div") as HTMLElement;
+    middleDivStepTwo.id = 'middle-div-step-two';
+    let youPickedContainer = document.createElement("div") as HTMLElement;
+    youPickedContainer.id = 'you-picked-container';
+   
+    let theHousePickedContainer = document.createElement("div") as HTMLElement;
+    theHousePickedContainer.id = 'the-house-picked-container';
+    
+    //inside containers-titles
+    let youPickedTitle = document.createElement("div") as HTMLElement;
+    youPickedTitle.id = 'you-picked-title';
+    youPickedTitle.innerHTML = 'you picked';
+    let theHousePickedTitle = document.createElement("div") as HTMLElement;
+    theHousePickedTitle.id = 'the-house-picked-title';
+    theHousePickedTitle.innerHTML = 'The house picked';
+    //inside containers-logos
+    let youPickedLogo = document.createElement("img") as HTMLElement;
+    youPickedLogo.className = 'button-step-two';
+    let yourChoiseInNumber:number;
+    switch(yourChoise) { 
+        case rock: { 
+            youPickedLogo.style.borderColor = 'hsl(349, 71%, 52%)';
+            youPickedLogo.src = "./images/icon-rock.svg"
+
+            yourChoiseInNumber=1
+           break; 
+        } 
+        case paper: { 
+            youPickedLogo.style.borderColor = 'hsl(230, 89%, 62%)';
+            youPickedLogo.src = "./images/icon-paper.svg"
+
+            yourChoiseInNumber=2
+           break; 
+        } 
+        case scissors: { 
+            youPickedLogo.style.borderColor = 'rgb(255, 170, 0)';
+            youPickedLogo.src = "./images/icon-scissors.svg"
+            yourChoiseInNumber=3
+           break; 
+        } 
+     } 
+    // if(yourChoise===scissors){
+    //     youPickedLogo.style.borderColor = 'rgb(255, 170, 0)';
+    //     youPickedLogo.style.borderColor = 'hsl(230, 89%, 62%)';
+    //     youPickedLogo.style.borderColor = 'hsl(349, 71%, 52%)';
+    //     // youPickedLogo.src = './images/icon-scissors.svg'
+    //     // youPickedLogo.innerHTML = `<${yourChoice}>`
+    // }
+    let theHousePickedLogo = document.createElement("img") as HTMLElement;
+    theHousePickedLogo.className = 'button-step-two';
+    let computerChoice:number = Math.round(Math.random() * (3 - 1) + 1);
+    console.log(computerChoice);
+    if(computerChoice===1){
+        theHousePickedLogo.style.borderColor = 'hsl(349, 71%, 52%)';
+        theHousePickedLogo.src = "./images/icon-rock.svg"
+        // theHousePickedLogo.width = '22'
+    }
+    if(computerChoice===2){
+        theHousePickedLogo.style.borderColor = 'hsl(230, 89%, 62%)';
+        theHousePickedLogo.src = "./images/icon-paper.svg"
+    }
+    if(computerChoice===3){
+        theHousePickedLogo.style.borderColor = 'rgb(255, 170, 0)';
+        theHousePickedLogo.src = "./images/icon-scissors.svg"
+
+    }
+   
+    //#########################calulating win and points#################
+    if(yourChoiseInNumber===computerChoice){
+        let message:string = `It's a tie`;
+    }
+    if(yourChoiseInNumber===1 && computerChoice===2){
+        let message:string = `you lose`;
+        scores-=1;
+    }
+    if(yourChoiseInNumber===2 && computerChoice===3){
+        let message:string = `you lose`;
+        scores-=1;
+    }
+    if(yourChoiseInNumber===3 && computerChoice===1){
+        let message:string = `you lose`;
+        scores-=1;
+    }
+    if(yourChoiseInNumber===2 && computerChoice===1){
+        let message:string = `you win`;
+        scores+=1;
+    }
+    if(yourChoiseInNumber===3 && computerChoice===2){
+        scores+=1;
+    }
+    if(yourChoiseInNumber===1 && computerChoice===3){
+        scores+=1;
+    }
+    middleDiv?.appendChild(middleDivStepTwo);
+    middleDivStepTwo?.appendChild(youPickedContainer);
+    if (scores>=2){
+        let youWinContainer = document.createElement("div") as HTMLElement;
+        youWinContainer.id = 'you-win-container';
+        youWinContainer.innerHTML = 'YOU WIN'
+        middleDivStepTwo?.appendChild(youWinContainer);
+     }
+    middleDivStepTwo?.appendChild(theHousePickedContainer);
+    youPickedContainer?.appendChild(youPickedTitle);
+    youPickedContainer?.appendChild(youPickedLogo);
+    theHousePickedContainer?.appendChild(theHousePickedTitle);
+    theHousePickedContainer?.appendChild(theHousePickedLogo);
+console.log(scores);
+ 
+    
+}
+
